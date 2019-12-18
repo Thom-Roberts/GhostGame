f32a7aa7e9cd35220457d04929b6223c91ac1872
Thom-Roberts <robertsorange@gmail.com>
2019-12-18 11:38:20 -0700

    First

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float xOffset;
+
+    private void Start()
+    {
+        UpdateCameraPosition();
+    }
+
+    void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        transform.position = new Vector3(playerTransform.position.x + xOffset, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
