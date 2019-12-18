3433e59fbe1f1ee56d106530e762d08a5fc06ba5
Thom-Roberts <robertsorange@gmail.com>
2019-12-18 13:37:26 -0700

    Moving player via Rigidbody

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float acceleration;
+    public float maxSpeed;
+    public float slowDownFactor;
+    private Rigidbody rb;
 
-    void Update()
+    void Start()
     {
-        if(Input.GetKey(KeyCode.RightArrow))
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Vector3 force = Vector3.right * acceleration;
+            Vector3 velocity = rb.velocity + force;
+            rb.velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
         }
-        else if(Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            Vector3 force = Vector3.left * acceleration;
+            Vector3 velocity = rb.velocity + force;
+            rb.velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        }
+        else
+        {
+            // slow down the object
+            rb.velocity *= slowDownFactor;
         }
     }
 }
